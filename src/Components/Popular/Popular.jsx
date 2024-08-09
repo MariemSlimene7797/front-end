830bbb328aeef684b35c15f9c351bc1346758511
mariem7797 <mariem.slimene7797@gmail.com>
2024-08-09 14:27:38 +0100

    feat:add css of popular section

--- src/Components/Popular/Popular.jsx
+++ src/Components/Popular/Popular.jsx
@@ -12,8 +12,8 @@ export const Popular = () => {
             {data_product.map((item,i)=>{
                 return <Item key={i} id={item.id}
                 name={item.name} image={item.image}
-                new-price={item.new_price}
-                old-price={item.old_price}/>
+                new_price={item.new_price}
+                old_price={item.old_price}/>
             })}
 
         </div>
